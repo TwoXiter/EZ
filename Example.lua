a0126c04d0ba71a937a6a984ea4b88575faf9802
XToshy <xiterdev7@gmail.com>
2024-09-14 12:04:17 -0300

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,25 @@
+local Theme = "Maggix"
+local KeySys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoXiter/KeySystem/main/Themes/".. Theme.. ".lua"))()
+
+KeySys.Main({
+  HubName = "Zyne Hub",
+  Service = "zyne",
+  Discord = "https://discord.gg/codexlol",
+  CustomTextBoxMessage = nil, --put a string if u want
+  KeylessSettings = {
+  	LoopKeyless = {
+  	  Active = true,
+        Interval = 5
+       },
+        CorrectHWID = "Loading Script...",
+        IncorrectHWID = "INCORRECT HWID",
+       PremiumEnabled = false,
+       SavePremium = true 
+   },
+  NormalScript = function()
+    print("this is normal script")
+  end,
+  PremiumScript = function()
+    print("this is a premium script")
+  end
+})
